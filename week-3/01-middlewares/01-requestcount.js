dda552e64c1ec6c6003aa0d105e834faf0be27b8
nivesh <niveshnagar23@gmail.com>
2024-01-03 08:11:09 +0530

    solved 1 and 2 of week 3

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 
 const app = express();
 let requestCount = 0;
@@ -10,15 +10,25 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+// defining the middleware
+const reqCountMiddleware = (req, res, next) => {
+  requestCount++;
+  console.log(requestCount);
+  next();
+};
+
+// using the middleware
+app.use(reqCountMiddleware);
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
-app.get('/requestCount', function(req, res) {
+app.get("/requestCount", function (req, res) {
   res.status(200).json({ requestCount });
 });
 
--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,9 @@
-const jwt = require('jsonwebtoken');
-const jwtPassword = 'secret';
+const jwt = require("jsonwebtoken");
+const z = require("zod");
+const jwtPassword = "secret";
 
+const usernameSchema = z.string().email();
+const passwordSchema = z.string().min(6);
 
 /**
  * Generates a JWT for a given username and password.
@@ -14,7 +17,14 @@ const jwtPassword = 'secret';
  *                        the password does not meet the length requirement.
  */
 function signJwt(username, password) {
-    // Your code here
+  const isUsernameValid = usernameSchema.safeParse(username).success;
+  const isPasswordValid = passwordSchema.safeParse(password).success;
+
+  if (isUsernameValid && isPasswordValid) {
+    const token = jwt.sign({ username, password }, jwtPassword);
+    return token;
+  }
+  return null;
 }
 
 /**
@@ -26,7 +36,12 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+  try {
+    jwt.verify(token, jwtPassword);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 /**
@@ -37,9 +52,17 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+  try {
+    const decoded = jwt.decode(token);
+    if (decoded) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch {
+    return false;
+  }
 }
-
 
 module.exports = {
   signJwt,
